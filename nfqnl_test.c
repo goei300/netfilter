624a0af8e6745d6951871300c4ea54e0121e7f37
MONTH4 <goei4559@gmail.com>
2023-08-16 21:40:22 +0900

    netfilter_argument_site
    유해사이트 필터

--- nfqnl_test.c
+++ nfqnl_test.c
@@ -11,6 +11,18 @@
 
 /* returns packet id */
 
+unsigned char* harmfulSite = NULL;
+unsigned char* host_str=NULL;
+int check=0;
+void dump2(unsigned char* buf, int size) {
+	int i;
+	for (i = 0; i < size; i++) {
+		if (i != 0 && i % 16 == 0)
+			printf("\n");
+		printf("%02X ", buf[i]);	
+	}
+	printf("\n");
+}
 
 unsigned char* get_http_start_address(unsigned char* buf) {
     // Extract IP header length from the IHL field (lower 4 bits of first byte)
@@ -18,7 +30,6 @@ unsigned char* get_http_start_address(unsigned char* buf) {
 
     // Extract TCP header length (data offset) from the 13th byte of TCP header
     int tcp_header_len = ((buf[ip_header_len + 12] >> 4) & 0x0F) * 4;
-
     // Calculate HTTP start address
     unsigned char* http_start = buf + ip_header_len + tcp_header_len;
 
@@ -27,7 +38,6 @@ unsigned char* get_http_start_address(unsigned char* buf) {
 
 bool isHttp(unsigned char* buf, int size) {
     unsigned char* http_start = get_http_start_address(buf);
-
     if (http_start - buf >= size) {
         return false;
     }
@@ -36,6 +46,7 @@ bool isHttp(unsigned char* buf, int size) {
         "GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH", "CONNECT"
     };
     for (int i = 0; i < sizeof(methods) / sizeof(methods[0]); i++) {
+		
         if (strncmp((char*)http_start, methods[i], strlen(methods[i])) == 0) {
             return true;
         }
@@ -44,7 +55,14 @@ bool isHttp(unsigned char* buf, int size) {
     return false;
 }
 
-void dump(unsigned char* buf, int size) {
+bool isHarmful(unsigned char* site, unsigned char* hSite){
+	if(strncmp(site,hSite,strlen(hSite)+1)==0){
+		return true;
+	}
+	return false;
+}
+
+unsigned char* dump(unsigned char* buf, int size) {
 
 	unsigned char* http_start=get_http_start_address(buf);
 
@@ -59,8 +77,13 @@ void dump(unsigned char* buf, int size) {
 	
 	http_start= http_start + start_idx + 6; //host index pass
 
-	unsigned char host_str[12];
-	memset(host_str, 0, sizeof(host_str));  // Initialize it to zeros
+
+    unsigned char* host_str = malloc(12);  // Dynamically allocate memory
+    if (!host_str) {
+        return NULL;  // Memory allocation failed
+    }
+    memset(host_str, 0, 12);  
+	
 	for(int i=0;i<strlen((char*)http_start);i++){
 		if(http_start[i]==0x0d && http_start[i+1]==0x0a){
 			break;
@@ -69,6 +92,8 @@ void dump(unsigned char* buf, int size) {
 	}
 
     printf("net is %s\n\n", host_str);
+
+	return host_str;
 }
 
 
@@ -84,47 +109,51 @@ static u_int32_t print_pkt (struct nfq_data *tb)
 	ph = nfq_get_msg_packet_hdr(tb);
 	if (ph) {
 		id = ntohl(ph->packet_id);
-		printf("hw_protocol=0x%04x hook=%u id=%u ",
-			ntohs(ph->hw_protocol), ph->hook, id);
+/* 		printf("hw_protocol=0x%04x hook=%u id=%u ",
+			ntohs(ph->hw_protocol), ph->hook, id); */
 	}
 
 	hwph = nfq_get_packet_hw(tb);
 	if (hwph) {
 		int i, hlen = ntohs(hwph->hw_addrlen);
 
-		printf("hw_src_addr=");
-		for (i = 0; i < hlen-1; i++)
+/* 		printf("hw_src_addr="); */
+/* 		for (i = 0; i < hlen-1; i++)
 			printf("%02x:", hwph->hw_addr[i]);
-		printf("%02x ", hwph->hw_addr[hlen-1]);
-	}
+		printf("%02x ", hwph->hw_addr[hlen-1]); */
+	} 
 
 	mark = nfq_get_nfmark(tb);
-	if (mark)
-		printf("mark=%u ", mark);
+/* 	if (mark)
+		printf("mark=%u ", mark); */
 
 	ifi = nfq_get_indev(tb);
-	if (ifi)
-		printf("indev=%u ", ifi);
+/* 	if (ifi)
+		printf("indev=%u ", ifi); */
 
 	ifi = nfq_get_outdev(tb);
-	if (ifi)
-		printf("outdev=%u ", ifi);
+/* 	if (ifi)
+		printf("outdev=%u ", ifi); */
 	ifi = nfq_get_physindev(tb);
-	if (ifi)
-		printf("physindev=%u ", ifi);
+/* 	if (ifi)
+		printf("physindev=%u ", ifi); */
 
 	ifi = nfq_get_physoutdev(tb);
-	if (ifi)
-		printf("physoutdev=%u ", ifi);
+/* 	if (ifi)
+		printf("physoutdev=%u ", ifi); */
 
 	ret = nfq_get_payload(tb, &data);
-	if (ret >= 0)
-		printf("payload_len=%d\n", ret);
-	
+/* 	if (ret >= 0)
+		printf("payload_len=%d\n", ret); */
+
 	if(isHttp(data,ret)){
-    	dump(data,ret);
+    	host_str=dump(data,ret);
+		check=1;
+		printf("is here? %s \n\n",host_str);
 	}
-	fputc('\n', stdout);
+
+
+/* 	fputc('\n', stdout); */
 
 	return id;
 }
@@ -137,8 +166,20 @@ static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
 	      struct nfq_data *nfa, void *data)
 {
 	u_int32_t id = print_pkt(nfa);
-	printf("entering callback\n");
-	return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
+/* 	printf("entering callback\n"); */
+
+	printf("log!\n");
+	if (check==1&& isHarmful(host_str, harmfulSite)) {
+    	printf("Harmful site detected: %s\n", host_str);
+		
+    	return nfq_set_verdict(qh, id, NF_DROP, 0, NULL); // Drop packet
+	}
+	else{
+		return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
+	}
+	check=0;
+	host_str=NULL;
+
 }
 
 
@@ -150,7 +191,12 @@ int main(int argc, char **argv)
 	int fd;
 	int rv;
 	char buf[4096] __attribute__ ((aligned));
-
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s <harmful_site>\n", argv[0]);
+		exit(1);
+	}
+	harmfulSite= (unsigned char*)argv[1];
+	printf("argu is %s\n",harmfulSite);
 	printf("opening library handle\n");
 	h = nfq_open();
 	if (!h) {
@@ -187,7 +233,7 @@ int main(int argc, char **argv)
 
 	for (;;) {
 		if ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0) {
-			printf("pkt received\n");
+/* 			printf("pkt received\n"); */
 			nfq_handle_packet(h, buf, rv);
 			continue;
 		}
